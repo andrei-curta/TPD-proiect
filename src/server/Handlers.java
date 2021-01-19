80352c5e4214df24333032e23ea492d9c9d6a277
Andrei Curta <curta.andrei98@gmail.com>
2021-01-19 17:41:22 +0200

    reparat hibernate

--- src/server/Handlers.java
+++ src/server/Handlers.java
@@ -2,6 +2,11 @@ package server;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import entities.UserEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import util.HibernateUtil;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,6 +18,28 @@ public class Handlers {
         @Override
         public void handle(HttpExchange he) throws IOException {
             System.out.println("intra");
+            UserEntity user = new UserEntity();
+//
+            try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+                user = session.get(UserEntity.class, 1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            SessionFactory factory;
+
+            try {
+                factory = new Configuration().configure().buildSessionFactory();
+                Session session = factory.openSession();
+                user = session.get(UserEntity.class, new  Long(1));
+            } catch (Throwable ex) {
+                System.err.println("Failed to create sessionFactory object." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+
+            System.out.println(user.getUsername());
+
             String response = "<h1>Server start success if you see this message</h1>" + "<h1>Port: " + "</h1>";
             he.sendResponseHeaders(200, response.length());
             OutputStream os = he.getResponseBody();
